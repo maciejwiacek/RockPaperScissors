d9e4d340a060ed4e1b43ee0778380772510078ee
Maciej Wiącek <maciejwiacek4@gmail.com>
2023-08-19 00:31:20 +0200

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Maciej Wiącek on 19/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
